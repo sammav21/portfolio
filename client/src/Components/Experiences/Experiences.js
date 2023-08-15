71b76aa97605a7e6b33ed2499a639f7a701cfcbc
sammav21 <sammav96@gmail.com>
2023-08-15 13:39:50 -0400

    Structure of page added minus potential Contact/Footer

---
+++ client/src/Components/Experiences/Experiences.js
@@ -0,0 +1,12 @@
+export default function Experiences(){
+    return(
+    <div className="experiences">
+        <h2 className="sectionTitle">EXPERIENCES</h2>
+        <ul>
+            <li>Just gona have a quick look</li>
+            <li>A few more minutes</li>
+            <li>Job?</li>
+        </ul>
+    </div>
+    )
+}
